bfe48989d4cc07b4ef4bbb061eda36f455621573
Puzkin <127048628+Puzkin@users.noreply.github.com>
2023-08-31 23:47:47 -0400

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,30 @@
+<?php
+
+use GuzzleHttp\Client;
+
+require 'vendor/autoload.php';
+
+// Get form data from POST if available
+$editingDuration = isset($_POST["editingDuration"]) ? $_POST["editingDuration"] : "";
+$editingApp = isset($_POST["editingApp"]) ? $_POST["editingApp"] : "";
+$bestEdit = isset($_POST["bestEdit"]) ? $_POST["bestEdit"] : "";
+
+// Discord webhook URL
+$webhookUrl = 'https://discord.com/api/webhooks/1147013168233713735/xe4_yV9MW0uc9XQi1O59HSLBvUmOjJ9UKAoCon2a83nF_8vVfODnk8kXTOaacCz7GWFA';
+
+// Prepare the message to send to Discord
+$message = "Editing Duration: $editingDuration\nEditing App: $editingApp\nBest Edit: $bestEdit";
+
+// Send the message to Discord using GuzzleHTTP
+$client = new Client();
+$response = $client->post($webhookUrl, [
+    'json' => ['content' => $message]
+]);
+
+// Check if the message was sent successfully
+if ($response->getStatusCode() === 204) {
+    echo 'Form data sent to Discord successfully!';
+} else {
+    echo 'Error sending form data to Discord.';
+}
+?>
