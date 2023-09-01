4a749b00cb4c5578c6a4e5b22a3dc3bdd75409ab
Puzkin <127048628+Puzkin@users.noreply.github.com>
2023-09-01 11:12:15 -0400

    Update submit.php

--- submit.php
+++ submit.php
@@ -4,27 +4,32 @@ use GuzzleHttp\Client;
 
 require 'vendor/autoload.php';
 
-// Get form data from POST if available
-$editingDuration = isset($_POST["editingDuration"]) ? $_POST["editingDuration"] : "";
-$editingApp = isset($_POST["editingApp"]) ? $_POST["editingApp"] : "";
-$bestEdit = isset($_POST["bestEdit"]) ? $_POST["bestEdit"] : "";
+// Check if the form was submitted via POST
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get form data
+    $editingDuration = isset($_POST["editingDuration"]) ? $_POST["editingDuration"] : "";
+    $editingApp = isset($_POST["editingApp"]) ? $_POST["editingApp"] : "";
+    $bestEdit = isset($_POST["bestEdit"]) ? $_POST["bestEdit"] : "";
 
-// Discord webhook URL
-$webhookUrl = 'https://discord.com/api/webhooks/1147013168233713735/xe4_yV9MW0uc9XQi1O59HSLBvUmOjJ9UKAoCon2a83nF_8vVfODnk8kXTOaacCz7GWFA';
+    // Discord webhook URL
+    $webhookUrl = 'https://discord.com/api/webhooks/1147013168233713735/xe4_yV9MW0uc9XQi1O59HSLBvUmOjJ9UKAoCon2a83nF_8vVfODnk8kXTOaacCz7GWFA';
 
-// Prepare the message to send to Discord
-$message = "Editing Duration: $editingDuration\nEditing App: $editingApp\nBest Edit: $bestEdit";
+    // Prepare the message to send to Discord
+    $message = "Editing Duration: $editingDuration\nEditing App: $editingApp\nBest Edit: $bestEdit";
 
-// Send the message to Discord using GuzzleHTTP
-$client = new Client();
-$response = $client->post($webhookUrl, [
-    'json' => ['content' => $message]
-]);
+    // Send the message to Discord using GuzzleHTTP
+    $client = new Client();
+    $response = $client->post($webhookUrl, [
+        'json' => ['content' => $message]
+    ]);
 
-// Check if the message was sent successfully
-if ($response->getStatusCode() === 204) {
-    echo 'Form data sent to Discord successfully!';
+    // Check if the message was sent successfully
+    if ($response->getStatusCode() === 204) {
+        echo 'Form data sent to Discord successfully!';
+    } else {
+        echo 'Error sending form data to Discord.';
+    }
 } else {
-    echo 'Error sending form data to Discord.';
+    echo 'Form not submitted.';
 }
 ?>
